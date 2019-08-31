b4405549f01dae787f381690d49c4b7a47acfa4c
harold <hard.electronic87@gmail.com>
2019-08-30 22:19:19 -0500

    Chat Finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -14,46 +14,46 @@ if (nombre === -1 || sala === -1) {
 }
 
 var usuario = {
-    nombre: nombre,
-    sala:sala
+    nombre,
+    sala
 }
 
-// escuchar
-socket.on('connect', function () {
-    console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function (resp) {
-        console.log('Usuarios conectados:', resp)
-    })
+socket.on('connect', function() {
+    //console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp)
+    });
 
 });
 
 // escuchar
-socket.on('disconnect', function () {
+socket.on('disconnect', function() {
+
     console.log('Perdimos conexión con el servidor');
+
 });
 
-
-// Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function (resp) {
-    console.log('respuesta server: ', resp);
-});
 
 // Escuchar información
-socket.on('crearMensaje', function (mensaje) {
-
-    console.log('Servidor:', mensaje);
+socket.on('crearMensaje', function(mensaje) {
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje,false)
+    scrollBottom()
 });
 
-//escuchar cuando un usuario entra o sale del chat
-socket.on('listaPersonas', function (mensaje) {
-    console.log(mensaje);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersonas', function(personas) {
+    //console.log(personas);
+    renderizarUsuarios(personas)
 });
 
-//Mensaje Provados
-socket.on('mensajePrivado', function (mensaje) {
-    console.log('Mensaje Privado: ', mensaje)
-})
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
